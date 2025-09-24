d364424b10aee722b933033681f75dfd351d2b9d
umer4472 <muhammadumerch1@gmail.com>
2025-09-24 13:34:30 +0300

    add logo title

--- app.py
+++ app.py
@@ -10,6 +10,11 @@ from plot_utils import plot_all_together
 set_page_config()
 inject_css()
 
+col1, col2 = st.columns([1, 6])
+with col1:
+    st.image("mobily_logo.png", width=80)
+with col2:
+    st.title("Network Availability Dashboard")
 # ------------------------
 # Load data
 # ------------------------
